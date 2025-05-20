f28eea838c823c7e1a0867c96a1c22328889a83d
六六 <hexsixology@gmail.com>
2025-05-20 10:25:21 +0800

    在AnimeList组件中添加抛弃率计算功能，显示每部动画的抛弃率，以百分比形式呈现。

--- components/AnimeList.vue
+++ components/AnimeList.vue
@@ -94,6 +94,12 @@
                 </svg>
                 评分: {{ getTotalRatingCount(anime) }}人
               </div>
+              <div class="text-xs text-gray-600 flex items-center">
+                <svg class="w-3 h-3 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"></path>
+                </svg>
+                抛弃率: {{ calculateDropRate(anime) }}%
+              </div>
               <div v-if="anime.air_date" class="text-xs text-gray-500">
                 {{ anime.air_date }}
               </div>
@@ -145,6 +151,12 @@
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"></path>
               </svg>
               评分: {{ getTotalRatingCount(anime) }}人
+            </div>
+            <div class="flex items-center">
+              <svg class="w-3 h-3 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"></path>
+              </svg>
+              抛弃率: {{ calculateDropRate(anime) }}%
             </div>
           </div>
           <div v-if="anime.air_date" class="text-xs text-gray-500">
@@ -206,6 +218,12 @@ export default {
              anime.collection.wish + 
              (anime.collection.on_hold || 0) + 
              (anime.collection.dropped || 0)
+    },
+    calculateDropRate(anime) {
+      if (!anime.collection) return 0
+      const totalCollection = this.getTotalCollectionCount(anime)
+      if (totalCollection === 0) return 0
+      return ((anime.collection.dropped || 0) / totalCollection * 100).toFixed(1)
     },
     getBroadcastDay(anime) {
       if (!anime.infobox || !Array.isArray(anime.infobox)) return null
