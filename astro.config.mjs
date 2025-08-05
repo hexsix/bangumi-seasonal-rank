9da13c54804eb7fc794d33b55f0b20d1f55de614
hexsix <hexsixology@gmail.com>
2025-08-05 23:50:31 +0800

    feat: header

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,16 @@ import { defineConfig } from 'astro/config';
 
 import icon from "astro-icon";
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.rinshankaiho.fun",
   base: "/",
   trailingSlash: "always",
   integrations: [icon()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
