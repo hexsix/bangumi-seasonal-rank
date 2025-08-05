053bc880b99141ac8aee122f5daf925af170e5bd
hexsix <hexsixology@gmail.com>
2025-08-05 20:36:38 +0800

    feat: add astro icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://www.rinshankaiho.fun",
-    base: "/",
-    trailingSlash: "always",
+  site: "https://www.rinshankaiho.fun",
+  base: "/",
+  trailingSlash: "always",
+  integrations: [icon()],
 });
